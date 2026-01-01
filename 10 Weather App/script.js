cd22e3a1d2ddf68c72995e994ca0b0c0e68e2501
Coderchaser <prachu11badekar@gmail.com>
2026-01-01 18:27:24 +0530

    Weather App

--- 10 Weather App/script.js
+++ 10 Weather App/script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById("weather-btn");
 const input = document.getElementById("text-input");
 const weatherDataEl = document.getElementById("weather-data");
 
-const API_KEY = "9eaf57de7c49ce2aa14f36c5805f268c";
+const API_KEY = "";
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
