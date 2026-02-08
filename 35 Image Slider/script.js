0a74341a74fc835d186ba87268e2abd77e4ab7ed
Coderchaser <prachu11badekar@gmail.com>
2026-02-08 21:14:30 +0530

    Image Slider added

---
+++ 35 Image Slider/script.js
@@ -0,0 +1,36 @@
+const images = [
+    "https://images.unsplash.com/photo-1770106678115-ec9aa241cdf6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1770236597921-7cdfdff367d9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1761839262867-af53d08b0eb5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2NHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1768985439879-a692f2742149?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D",
+    "https://images.unsplash.com/photo-1769399287827-4b7d79d99e0d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1767337962049-19cc90b600aa?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1768123441959-8a989dbb878e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1764617755316-ffb5ff87c2d7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1767714653229-a30599446f82?q=80&w=1218&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+];
+
+const containerEl = document.querySelector(".container");
+const figureEl = document.querySelector(".figure");
+const imageEl = document.querySelector("img");
+const btnEl = document.querySelector(".btn");
+
+console.log(containerEl);
+console.log(figureEl);
+console.log(imageEl);
+
+
+imageEl.src = images[0];
+
+btnEl.addEventListener("click", () => {
+    images.forEach(img => {
+        imageEl.src = img;
+        console.log(img);
+    });
+});
+
+
+
+
+
+
