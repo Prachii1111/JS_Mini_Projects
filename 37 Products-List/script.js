0db02fff2942090fcf1e0f1030dde1d7f25c8252
Coderchaser <prachu11badekar@gmail.com>
2026-02-19 21:10:20 +0530

    Add to cart feature

--- 37 Products-List/script.js
+++ 37 Products-List/script.js
@@ -3,6 +3,7 @@ const productContainer = document.querySelector(".product-container");
 const productList = document.createElement("div");
 productList.classList.add("product-list");
 
+let allProducts = [];
 let cart = [];
 
 async function fetchProducts() {
@@ -11,6 +12,7 @@ async function fetchProducts() {
         const products = await response.json();
 
         console.log(products);
+        allProducts = products;
 
         products.forEach(product => {
             const productCard = document.createElement("div");
@@ -23,13 +25,14 @@ async function fetchProducts() {
                 <div class="product-content">
                     <h2>${product.name}</h2>
                     <p>₹${product.price}</p>
-                    <button class="addToCart">Add to Cart</button>
+                    <button class="addToCart" data-id="${product.id}">Add to Cart</button>
                 </div>
             `
 
             productList.appendChild(productCard);
-            productContainer.appendChild(productList);
         })
+
+        productContainer.appendChild(productList);
 
     } catch(error) {
         console.log("Error Fetching data", error);
@@ -38,6 +41,73 @@ async function fetchProducts() {
 
 fetchProducts();
 
-productList.addEventListener("click", () => {
-    
+const cartModal = document.createElement("div");
+cartModal.classList.add("cart-modal");
+
+
+productList.addEventListener("click", (e) => {
+    console.log(e.target);
+
+    if(e.target.classList.contains("addToCart")) {
+        
+        const productId = Number(e.target.dataset.id);
+
+        // Find product from original list 
+        const selectedProduct = allProducts.find(
+            p => p.id === productId
+        );
+
+        // check if already in cart 
+        const existingItem = cart.find(
+            item => item.id === productId
+        );
+
+        if(existingItem) {
+            existingItem.quantity += 1; 
+        } else {
+            cart.push({
+                ...selectedProduct, 
+                quantity: 1 
+            });
+        }
+        console.log(cart);
+
+        const CartTotal = updateTotal();
+        console.log("CartTotal:", CartTotal);
+
+        // rendering selected items 
+        cartModal.innerHTML = "";   // cleart first
+        cartModal.innerHTML = `
+        <h2 class="total-items">Your Cart  (${cart.length})</h2>`;
+
+        cart.forEach(item => {
+            cartModal.innerHTML += `
+                <div class="cart-items">
+                    <h2>${item.name}</h2>
+                    <div class="cart-nos">
+                        <strong>${item.quantity}x</strong>
+                        <span>₹${item.price}</span>
+                    </div>
+                </div>
+        `
+        })
+
+        cartModal.innerHTML += `
+            <div class="final-total">
+                <span>Order Total</span>
+                <strong>₹${CartTotal}</strong
+            </div>
+        `
+        productContainer.appendChild(cartModal);
+    }
 })
+
+function updateTotal() {
+    const total = cart.reduce((sum, item) => {
+        return sum + item.price * item.quantity;
+    }, 0);
+
+    return total;
+    // console.log("Total:", total);
+}
+
